1dd4207308af318e3ad17d4cb5202f070f03e8bd
alexa <alex@targoninc.com>
2024-06-19 23:03:42 +0200

    more fun

--- fun.mjs
+++ fun.mjs
@@ -8,6 +8,15 @@ const dataPath = path.join(process.env.DISCORD_DATA_PATH, "messages.json");
 const data = fs.readFileSync(dataPath, "utf8");
 const json = JSON.parse(data);
 
+const confidenceTreshholds = [0.5, 0.6, 0.7, 0.8, 0.9];
+const countUnderTreshholds = confidenceTreshholds.map(treshhold => {
+    return json.filter(i => i.confidence < treshhold).length;
+});
+for (let i = 0; i < confidenceTreshholds.length; i++) {
+    const percentage = countUnderTreshholds[i] / json.length * 100;
+    console.log(`Count under ${confidenceTreshholds[i]}: ${countUnderTreshholds[i]} | ${percentage.toFixed(2)}%`);
+}
+
 const mostPositiveCount = 10;
 const mostPositive = json.sort((a, b) => b.weightedScore - a.weightedScore).slice(0, mostPositiveCount).map(i => {
     return {
