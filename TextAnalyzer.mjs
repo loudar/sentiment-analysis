ad5bd50efd71c58c7daaa3d2c195f4ba132c8dac
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2024-06-19 16:36:36 +0200

    basic averaging

--- TextAnalyzer.mjs
+++ TextAnalyzer.mjs
@@ -1,11 +1,39 @@
-import {AzureKeyCredential, TextAnalyticsClient} from "@azure/ai-text-analytics";
+const {AzureKeyCredential, TextAnalyticsClient} = require("@azure/ai-text-analytics");
 
 export class TextAnalyzer {
     constructor() {
         this.client = new TextAnalyticsClient(process.env.TEXT_ANALYTICS_ENDPOINT, new AzureKeyCredential(process.env.TEXT_ANALYTICS_KEY));
     }
 
-    async sentiment(text) {
-        return this.client.analyzeSentiment([ text ]);
+    async sentiment(input, language = "en") {
+        if (input.constructor === Array) {
+            let output = [];
+            for (const text of input) {
+                output.push(await this.sentiment(text));
+            }
+            return output;
+        }
+
+        input = input.replace(/\s+/g, ' ').trim();
+        if (input.length === 0) {
+            return 0;
+        }
+        const result = await this.client.analyzeSentiment([{
+            id: "1",
+            text: input,
+            language: language || "en"
+        }]);
+        const first = result[0];
+        return {
+            score: TextAnalyzer.sentimentValueMap[first.sentiment],
+            confidence: first.confidenceScores[first.sentiment]
+        };
     }
+
+    static sentimentValueMap = {
+        negative: -1,
+        positive: 1,
+        neutral: 0,
+        mixed: 0
+    };
 }
--- main.mjs
+++ main.mjs
@@ -5,8 +5,23 @@ dotenv.config();
 import { TextAnalyzer } from './TextAnalyzer.mjs';
 
 console.log(`Using endpoint ${process.env.TEXT_ANALYTICS_ENDPOINT}`);
-console.log(`Using key ${process.env.TEXT_ANALYTICS_KEY}`);
 const analyzer = new TextAnalyzer();
 
-const result = await analyzer.sentiment("I love this product");
-console.log(result);
+const text = `this is actually sick
+this allows me to git commit and have it rebuild on the system
+actually super cool
+this is what i always wanted
+instead of the in-between step`;
+
+const sentences = text.split('\n');
+const language = "en";
+
+const result = await analyzer.sentiment(sentences, language);
+console.log(sentences.map((text, index) => `${text} -> ${JSON.stringify(result[index])}`).join('\n'));
+
+const weightedScores = result.map(({ score, confidence }) => score * confidence);
+const average = weightedScores.reduce((a, b) => a + b, 0) / weightedScores.length;
+console.log(`Average sentiment: ${average}`);
+
+const median = weightedScores.sort()[Math.floor(weightedScores.length / 2)];
+console.log(`Median sentiment: ${median}`);
